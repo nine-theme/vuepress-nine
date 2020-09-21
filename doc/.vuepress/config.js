774db21b0e29dc4b06e2c8ee69951a1585c5f228
NineSwordsMonster <wangjia_1919@163.com>
2020-09-21 17:42:13 +0800

    WIP: style(vuepress-theme-nine): docs(vuepress-theme-nine): feat:(doc) 新增 changelog
    新增 changelog

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -8,16 +8,17 @@ module.exports = {
   ],
   locales: {
     '/': {
-      lang: 'zh-CN'
+      lang: 'zh-CN',
+      sidebar: 'auto'
     }
   },
-  theme: require.resolve('../..'),
+  theme: require.resolve('../../packages/vuepress-theme-nine'),
   themeConfig: {
     type: 'blog',
     mode: 'auto',// 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
     nav: [
-      { text: '主页', link: '/', icon: 'nine-home' },
-      { text: '时间轴', link: '/timeline/', icon: 'nine-date' }
+      { text: '主页', link: '/', icon: 'home' },
+      { text: '时间轴', link: '/timeline/', icon: 'hourglass-half' }
     ],
     sidebar: 'auto',
     // 博客设置
@@ -79,6 +80,30 @@ module.exports = {
     /**
      * 使用3D tag 标签
      */
-    user3DTag: true
+    user3DTag: true,
+    vssueConfig: {
+      platform: 'github',
+      owner: 'NineSwordsMonster',
+      repo: 'gitment-comments',
+      clientId: '6df5e3f1c9df36da16f8',
+      clientSecret: 'b1e8d8bf0f60f1cffd7aedfed12e2383553028ea',
+    },
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'NineSwordsMonster',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'GitHub',
+
+    // 以下为可选的编辑链接选项
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'nine-theme/nine-theme',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'doc',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！'
   }
 }
